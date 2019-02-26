1767849ce064e9f926f1136a365120610d13ef1a
Jose Felix <jfelix000@west-mec.org>
2019-02-25 18:21:04 -0700

    started if statement
    Today we worked on the calWeekdayRow function and got the calendar populated with actual <th>. We left off working on the daysInMonth function to get every day in there and work with the leap year.

--- lht_calendar.js
+++ lht_calendar.js
@@ -8,7 +8,7 @@
    Author: Jose Felix
    Date:  2.19.19
 
-   Filename:   lht_calendar.js  
+   Filename:   lht_calendar.js
 
    Function List:
    createCalendar(calDate)
@@ -29,7 +29,6 @@
       Writes the daily rows in the calendar table, highlighting calDate
 	
 */
-
 // Set the date displayed in the calendar
 var thisDay = new Date("August 24, 2018");
 
@@ -40,6 +39,7 @@ document.getElementById("calendar").innerHTML = createCalendar(thisDay);
 function createCalendar(calDate) {
       var calendarHTML = "<table id='calendar_table'>";
       calendarHTML += calCaption(calDate);
+      calendarHTML += calWeekdayRow();
       calendarHTML += '</table>';
       return calendarHTML;
 }
@@ -58,6 +58,38 @@ function calCaption(calDate) {
       //Write the caption
       return "<caption>"
       + monthName[thisMonth] +
-      " " + thisYear + 
+      " " + thisYear +
       "</caption>";
+}
+
+// function to write a table row of weekday abbreviations
+function calWeekdayRow() {
+      //array of weekday abbreviations
+      var dayName = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+      var rowHTML = "<tr>";
+
+      //look through the day name array
+      for (var i = 0; i < dayName.length; i++) {
+            rowHTML += "<th class='calendar_weekdays'>" + dayName[i] + "</th>";
+      }
+      rowHTML += "</tr>";
+      return rowHTML;
+}
+
+//function to calculate the number of days in the month
+function daysInMonth(calDate)  {
+      //array of days in each month
+      var dayCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+      //extract the 4 digit year and month value
+      var thisYear = calDate.getFullYear();
+      var thisMonth = calDate.getMonth();
+
+      //revise the days in February for leap years
+      if (thisYear % 4 === 0) {
+            dayCount[1] = 29;
+      }
+
+      //return the number of days for the current month
+      return dayCount[thisMonth];
 }
